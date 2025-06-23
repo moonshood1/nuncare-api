b4d51d2716429e4c853861849e7e8cc7926b8760
Louis Roger GUIRIKA <l.guirika@panelyscash.local>
2025-06-23 10:42:24 +0000

    added fix to user notifications routes

--- src/routes/resources/controller/getNotifications.js
+++ src/routes/resources/controller/getNotifications.js
@@ -3,8 +3,8 @@ const Notification = require("../../../models/Notification");
 const getNotifications = async ({ user }, res, next) => {
   try {
     const notifications = await Notification.find({
-      users: { $nin: [user._id] },
-    });
+      users: { $in: [user._id] },
+    }).select("-users");
 
     return res.status(200).json({
       success: true,
